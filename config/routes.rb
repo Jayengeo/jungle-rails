bcf2984d10285da04723b24335c6d4e4b4e55073
Jacky Ngo <jacky.ngo96@gmail.com>
2023-04-12 12:06:01 -0400

    Create about page and provide links in the nav bars

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,9 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
-  end
+  end 
+
+  get '/about' => 'about#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
