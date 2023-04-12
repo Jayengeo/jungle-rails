211524c2e9168d77e2d7e43d44fb63f2aec94375
Jacky Ngo <jacky.ngo96@gmail.com>
2023-04-12 18:32:13 -0400

    Create categories index page

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,10 +1,13 @@
 class Admin::CategoriesController < ApplicationController
   def index
+    @categories = Category.all
   end
 
   def new
+    @category = Product.new
   end
 
   def create
+    @category = Category.new(category_params)
   end
 end
