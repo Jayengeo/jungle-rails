5769b56e0ff85664912da253e59faedbc22a4284
Jacky Ngo <jacky.ngo96@gmail.com>
2023-04-13 16:47:57 -0400

    generate appropriate user controller, model and view files

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,10 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :new, :create]
   end 
 
-  get '/about' => 'about#show'
+  get '/about' => 'about#show' 
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
